9a68d001a14a1e391326eb7bf921b6bfb9ea6076
stevejamerson <stevejamerson@outlook.com>
2021-12-09 02:32:57 -0300

    feat: icon

---
+++ src/components/Atoms/Icon/Icon.tsx
@@ -0,0 +1,20 @@
+import { ElementType, FC } from "react";
+import IconsTypes, { IconName } from "./Icon.types";
+
+interface IconProps {
+    component?: ElementType;
+    size?: number;
+    name: IconName;
+}
+
+const Icon: FC<IconProps> = ({ name, size = 24, ...props }) => {
+    const Icon = IconsTypes[name];
+    return (
+        <Icon
+            width={size}
+            height={size}
+        />
+    )
+}
+
+export default Icon;
---
+++ src/components/Atoms/Icon/Icon.types.ts
@@ -0,0 +1,10 @@
+
+import { ReactComponent as Google } from './google.svg'
+
+const IconsTypes = {
+    google: Google
+}
+
+export default IconsTypes
+
+export type IconName = 'google';
